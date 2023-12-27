3b5b004ec58c3ca23b8fc72ed797f28bd1c3d00b
anhakobyan25 <150049080+anhakobyan25@users.noreply.github.com>
2023-12-27 10:01:33 +0400

    Add files via upload

---
+++ Homework7.js
@@ -0,0 +1,58 @@
+// filter
+function myFilter(array, fn) {
+  let filteredArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (fn(array[i], i, array)) {
+      filteredArray.push(array[i]);
+    }
+  }
+  return filteredArray;
+}
+
+//find
+function myFind(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    if (fn(array[i], i, array)) {
+      return array[i];
+    }
+  }
+  return undefined
+}
+
+
+//some
+function mySome(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    if (fn(array[i], i, array)) {
+      return true;
+    }
+  }
+  return false
+}
+
+
+//forEach
+function myForEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array)
+  }
+}
+
+//map
+function myMap(array, fn) {
+  let newArray = []
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(fn(array[i], i, array))
+  }
+  return newArray
+}
+
+//every
+function myEvery(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    if (!(fn(array[i], i, array))) {
+      return false;
+    }
+  }
+  return true
+}
