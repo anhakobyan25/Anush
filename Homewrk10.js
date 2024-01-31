d849ada58839e632b4385957195f0f1501ddb7a3
anhakobyan25 <150049080+anhakobyan25@users.noreply.github.com>
2024-01-31 21:46:01 +0400

    Update Homewrk10.js

--- Homewrk10.js
+++ Homewrk10.js
@@ -1,10 +1,14 @@
-function changePinCode(newPinCode){
-	this.pinCode =  newPinCode
+function changePinCode(newPinCode, oldPin){
+if(oldPin === this.pinCode){
+	this.pinCode = newPinCode
+}
+	return "stacvec"
 }
  
 function paymentToOtherAccount(cardToSend, amount){
-      if (this.balance >= amount) {
+      if (this.balance >= amount &&  cardToSend instanceof Card){}
         this.balance =  this.balance - amount
+				cardToSend.balance = cardToSend.balance + amount - amount * .5
       } else {
         console.log("ԱՆբավարար միջոցներ")
       }
@@ -14,11 +18,11 @@ function addBalance (amount){
 	this.balance = this.balance + amount
 }
 
-function Card(id, name, surname, expDate, pinCode, accNumber,balance){
-  this.id =  id  //Math.floor(Math.random() * 100000)
+function Card( name, surname, expDate, pinCode, accNumber,balance){
+  this.id =  Math.floor(Math.random() * 100000)
 	this.name= name
 	this.surname = surname
-	this.expDate = expDate
+	this.expDate = '01/2027'
 	this.pinCode = pinCode
 	this.changePinCode = changePinCode
 	this.accNumber = accNumber
@@ -26,4 +30,35 @@ function Card(id, name, surname, expDate, pinCode, accNumber,balance){
 	this.paymentToOtherAccount = paymentToOtherAccount
   this.addBalance = addBalance
 }
+function changePinCode(newPinCode, oldPin){
+if(oldPin === this.pinCode){
+	this.pinCode = newPinCode
+}
+	return "stacvec"
+}
+ 
+function paymentToOtherAccount(cardToSend, amount){
+      if (this.balance >= amount &&  cardToSend instanceof Card){}
+        this.balance =  this.balance - amount
+	cardToSend.balance = cardToSend.balance + amount - amount * 0.5
+      } else {
+        console.log("ԱՆբավարար միջոցներ")
+      }
+    }
 
+function addBalance (amount){
+	this.balance = this.balance + amount
+}
+
+function Card( name, surname, expDate, pinCode, accNumber,balance){
+  this.id =  Math.floor(Math.random() * 100000)
+	this.name= name
+	this.surname = surname
+	this.expDate = '01/2027'
+	this.pinCode = pinCode
+	this.changePinCode = changePinCode
+	this.accNumber = accNumber
+	this.balance = balance
+	this.paymentToOtherAccount = paymentToOtherAccount
+  this.addBalance = addBalance
+}
